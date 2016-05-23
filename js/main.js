6a3573a1f1822f2408761503dab3780638add733
RemingtonMaxwell <remingtonmaxwell@att.net>
2016-05-23 09:38:58 -0700

    adde both code generators

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
 function webJSchemaToJava() {
-
+    var jschema = document.getElementById("jschemaInput").value;
+	var className=document.getElementById('className').value;
+	if(className ===""){
+	    alert("Please Enter a Class Name");
+	}
+	className=className.charAt(0).toUpperCase()+className.slice(1);
+	var output = generateAll(className, jschema);
+	document.getElementById('jschemaOutput').innerHTML = output;
 }
 
 function webJSchemaToJavascript() {
 	var jschema = document.getElementById("jschemaInput").value;
-	//var className = "MyClass"; // This should be updated before release
 	var className=document.getElementById('className').value;
 	className=className.charAt(0).toUpperCase()+className.slice(1);
 	var output = generateJavascriptForJSchema(jschema, className);
 	document.getElementById('jschemaOutput').innerHTML = output;
+}
+
+function generateCode(){
+    var codeType=document.getElementById('selectCode').value;
+    if(codeType === "Java 8"){
+        webJSchemaToJava();
+    }else{
+        webJSchemaToJavascript();
+    }
 }
 
 function webJSONToJSchema() {
